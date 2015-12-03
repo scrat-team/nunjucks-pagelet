c037ee947ea280c7069a112cdd5ab97d69f72dac
天猪 <liuyong.ly3@alibaba-inc.com>
2015-12-03 16:55:48 +0800

    feat(Tag): require支持直接给url, 无需 $id

--- test/lib/tags/require.test.js
+++ test/lib/tags/require.test.js
@@ -9,6 +9,8 @@ const util = require('../../util');
 describe('test/lib/tags/require.test.js', function() {
   let app, env;
 
+  const locals = {title: 'this is title', href: 'http://t.cn', deep: {foo: 'foo'}, override: {foo: 'override'}};
+
   before(function() {
     app = util('general');
     env = app.env;
@@ -17,8 +19,14 @@ describe('test/lib/tags/require.test.js', function() {
   after(util.restore);
 
   it('should render require tag ', function() {
-    const tpl = '{% html %}{% require $id="foo", title="this is title", href=href, deep=deep.foo %}{% endhtml %}';
-    const html = env.renderString(tpl, {title: 'this is title', href: 'http://t.cn', deep: {foo: 'foo'}});
+    const tpl = '{% html %}{% require $id="foo", title="this is title", href=href, deep=override %}{{deep.foo}}{% endhtml %}';
+    const html = env.renderString(tpl, locals);
+    expect(html).to.equal('<html><h1>this is title</h1>\n<a href="http://t.cn">ucweb</a>\n<span>override</span>foo</html>');
+  });
+
+  it('should render require tag with single url ', function() {
+    const tpl = '{% html %}{% require "foo" %}{% endhtml %}';
+    const html = env.renderString(tpl, locals);
     expect(html).to.equal('<html><h1>this is title</h1>\n<a href="http://t.cn">ucweb</a>\n<span>foo</span></html>');
   });
 });
