c9b7d5793aecaf1c86d8cb37dc33cdab26ac6787
George Chung <Gerhut@GMail.com>
2015-12-03 17:40:50 +0800

    add custom `parseDignature` without comma detection.

--- test/lib/tags/require.test.js
+++ test/lib/tags/require.test.js
@@ -21,6 +21,12 @@ describe('test/lib/tags/require.test.js', function() {
     expect(html).to.equal('<html><h1>this is title</h1>\n<a href="http://scrat.io">scrat</a>\n<span>override</span>foo</html>');
   });
 
+  it('should render require tag without comma ', function() {
+    const tpl = '{% html %}{% require $id="foo" title="this is title" href=href deep=override %}{{deep.foo}}{% endhtml %}';
+    const html = env.renderString(tpl, locals);
+    expect(html).to.equal('<html><h1>this is title</h1>\n<a href="http://t.cn">ucweb</a>\n<span>override</span>foo</html>');
+  });
+
   it('should render require tag with single url ', function() {
     const tpl = '{% html %}{% require "foo" %}{% endhtml %}';
     const html = env.renderString(tpl, locals);
